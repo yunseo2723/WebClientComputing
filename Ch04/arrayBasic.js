3e742c46cd32cc7584d2a2a3127d9c462da1d577
yunseo2723 <yunseo2723@naver.com>
2023-04-14 16:44:55 +0900

    Update

---
+++ Ch04/arrayBasic.js
@@ -0,0 +1,8 @@
+let array = [52, 273, '아침밥', true]
+
+array[0] = 0;
+
+for(let i=0; i<array.length+1; i++)
+    console.log(array[i]);
+
+console.log(array.length);
---
+++ Ch04/whileLoop.js
@@ -0,0 +1,8 @@
+let i = 0;
+let array = [52, 273, 32, 65, 103];
+
+while (i < array.length) {
+    console.log(i + "번째 출력: " + array[i]);
+
+    i++;
+}
